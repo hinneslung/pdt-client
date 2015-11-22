17725533313818906a9d887dd1d60c1ccfe82d0c
hinneslung <hinneslung@gmail.com>
2015-11-23 03:06:20 +0800

    metrics

--- js/app.js
+++ js/app.js
@@ -19,6 +19,13 @@
 		}
 	});
 
+    //filters
+    app.filter('percentage', ['$filter', function ($filter) {
+        return function (input, decimals) {
+            return $filter('number')(input * 100, decimals) + '%';
+        };
+    }]);
+
     //Routing
     app.config(function ($routeProvider, $locationProvider) {
 
--- js/project.js
+++ js/project.js
@@ -4,14 +4,17 @@ function productService() {
 	ps.processProject = function(originalProject) {
 		var project = originalProject;
 
+		//fix api order
 		var temp = project.phases[2];
 		project.phases[2] = project.phases[1];
 		project.phases[1] = temp;
 
+		//add title for UI
 		var phaseTitles = ['Inception', 'Elaboration', 'Construction', 'Transition'];
 		for (var i = 0; i < 4; i++)
 			project.phases[i].title = phaseTitles[i];
 
+		//status
 		for (i = 0; i < 4; i++) {
 			if (project.phases[i].is_active) {
 				project.phases[i].status = 'A';
@@ -25,6 +28,25 @@ function productService() {
 				project.phases[i].status = project.phases[i].end_dateTime ? 'E' : 'N';
 				for (j = 0; j < project.phases[i].iterations.length; j++)
 					project.phases[i].iterations[j].status = project.phases[i].iterations[j].end_dateTime ? 'E' : 'N';
+			}
+		}
+
+		//metrics
+		project.effort = 0;
+		for (i = 0; i < 4; i++) {
+			var phase = project.phases[i];
+			project.phases[i].effort = 0;
+			if (project.metrics.phase.hasOwnProperty(phase.phase_type)) {
+				for (var j = 0; j < phase.iterations.length; j++) {
+					var metrics = project.metrics.phase[phase.phase_type][j];
+					project.phases[i].iterations[j].sloc = metrics.lines_of_codes;
+					project.phases[i].iterations[j].effort = metrics.effort;
+
+					project.phases[i].sloc = metrics.lines_of_codes;
+					project.phases[i].effort += metrics.effort;
+				}
+				project.sloc = project.phases[i].sloc;
+				project.effort += project.phases[i].effort;
 			}
 		}
 
